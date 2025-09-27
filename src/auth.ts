65d596a732777f0cfdda85fbc66d860b5bb8d413
georgesamir172 <george.samir172@gmail.com>
2025-09-28 01:13:42 +0300

    4 commit

--- src/auth.ts
+++ src/auth.ts
@@ -7,8 +7,7 @@ export const authOptions : AuthOptions  = {
 // import email from './../node_modules/next-auth/core/lib/email/signin.d';
 //    import { User } from './../node_modules/next-auth/core/types.d';
  pages :{
-    // signIn:"/login"
-    signIn:process.env.NEXT_PUBLIC_BASE_URL + '/login'
+    signIn:"/login"
 },
  providers : [
        CredentialsProvider({
